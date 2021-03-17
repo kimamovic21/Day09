d595ffbfd884d5777f1f9c38e66081f8639b9ae2
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-17 09:45:08 +0100

    Add files via upload

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function compareDifferentValues (m,n ) {
+    if (m===n) {
+   return "Equal";
+    }
+    return "Not equal";
+}
+
+compareDifferentValues(8, "8");
+compareDifferentValues("8", 8);
+compareDifferentValues('8', 8);
+compareDifferentValues("8", "8");
+compareDifferentValues(8,8);
+module.exports = compareDifferentValues;
