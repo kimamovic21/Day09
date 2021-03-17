4679e8e0db1c6e8efa6fa1bb049104f0f7899476
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-17 09:46:11 +0100

    Add files via upload

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function functionElse (num) {
+if (num <= 5) {
+    return "5 or smaller";
+} else {
+    return "Bigger than 5";
+}
+}
+
+functionElse (4);
+functionElse (5);
+functionElse (6);
+functionElse (10);
+
+module.exports = functionElse;
