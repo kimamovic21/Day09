66eb33039668a47d8d26132f4faa90219de6848b
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-17 09:47:31 +0100

    Add files via upload

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+for ([initialization]; [condition]; [final-expression])
+function myForLoo1 ();
+for (var i = 0; i<7; i++) {
+    console.log(i);
+}
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+}
